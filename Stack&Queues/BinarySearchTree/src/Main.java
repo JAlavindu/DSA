8630ed6b8140489eb42ba85861baa6b2ee2059c3
lavindu perera <lavi156perera@gmail.com>
2025-04-12 22:32:28 +0530

    binary search tree

---
+++ Stack&Queues/BinarySearchTree/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(10);
+        myBST.insert(5);
+        myBST.insert(15);
+        myBST.insert(3);
+        myBST.insert(7);
+        myBST.insert(12);
+
+        System.out.println(myBST.root.value); // Should print 10
+        System.out.println(myBST.root.left.value); // Should print 5
+    }
+}
